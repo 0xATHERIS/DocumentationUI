c56abab252f36dabbf24dd1b15aae83f4afe2305
0xATHERIS <ath3ris@proton.me>
2023-05-11 00:13:46 +0100

    feat: :sparkles:  - Example of a Documentation page
    Documentation page that will use the UI components in 1 page to display how it would look

---
+++ src/stories/Documentation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from '../components/Article';
+
+function Documentation() {
+  return (
+    <main className="border">
+        <Article>
+            <h1>Test</h1>
+        </Article>
+    </main>
+  )
+}
+
+export default Documentation
