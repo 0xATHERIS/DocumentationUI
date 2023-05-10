b0086d431c2e2480ec799c4dce7ba0f6fd8a151e
0xATHERIS <ath3ris@proton.me>
2023-05-11 00:58:01 +0100

    refactor(Subtitle): :lipstick:  - reduced font-size and added margin bottom

--- src/components/Subtitle/Subtitle.tsx
+++ src/components/Subtitle/Subtitle.tsx
@@ -5,7 +5,7 @@ type SubtitleProps = {
 };
 
 const Subtitle = React.forwardRef<HTMLHeadingElement, SubtitleProps>(({ children, ...props }, ref) => (
-    <h4 ref={ref} className="text-2xl font-medium tracking-tight text-neutral-800" {...props}>
+    <h4 ref={ref} className="text-xl font-medium tracking-tight mb-4 text-neutral-800" {...props}>
         {children}
     </h4>
 ));
