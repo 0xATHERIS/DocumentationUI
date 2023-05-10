e271b3938704c84ccf297fe8788c0bd4b1eec986
0xATHERIS <ath3ris@proton.me>
2023-05-11 00:58:58 +0100

    feat(Title): :sparkles:  - concat className

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 type TitleProps = {
     children: React.ReactNode;
+    className?: string;
 };
 
-const Title = React.forwardRef<HTMLHeadingElement, TitleProps>(({ children, ...props }, ref) => (
-    <h1 ref={ref} className="text-4xl font-bold tracking-tight text-neutral-800" {...props}>
+const Title = React.forwardRef<HTMLHeadingElement, TitleProps>(({ children, className, ...props }, ref) => (
+    <h1 ref={ref} className={`text-4xl font-bold tracking-tight text-neutral-800 mt-4 mb-2 ${className}`} {...props}>
         {children}
     </h1>
 ));
