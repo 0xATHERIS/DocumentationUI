bfa3b6a06dc2bb955427b6d02fb547ac367b961e
0xATHERIS <ath3ris@proton.me>
2023-05-11 00:35:13 +0100

    refactor(Article): :lipstick:  - Added 1rem padding and passed remaining props to element

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -5,7 +5,7 @@ type ArticleProps = {
 };
 
 const Article = React.forwardRef<HTMLElement, ArticleProps>(({ children, ...props }, ref) => (
-    <article ref={ref} className="w-4/5 md:w-2/3 mx-auto">
+    <article ref={ref} className="w-4/5 md:w-2/3 mx-auto px-4" {...props}>
         {children}
     </article>
 ));
