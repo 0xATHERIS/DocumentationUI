134b3c5c283bf543055112eddf418210929ef4e2
0xATHERIS <ath3ris@proton.me>
2023-05-11 00:11:46 +0100

    feat(component): :sparkles:  - New Article component

---
+++ src/components/Article/Article.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+type ArticleProps = {
+    children: React.ReactNode;
+};
+
+const Article = React.forwardRef<HTMLElement, ArticleProps>(({ children, ...props }, ref) => (
+    <article ref={ref} className="w-4/5 md:w-2/3 mx-auto">
+        {children}
+    </article>
+));
+
+export default Article;
