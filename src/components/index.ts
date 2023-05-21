b38b567929823a9adbd2f754af9ad5302e4f9b74
0xATHERIS <ath3ris@proton.me>
2023-05-22 00:07:21 +0100

    feat: :sparkles:  - default exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as Article } from "./Article";
 export { default as Title } from "./Title";
 export { default as Subtitle } from "./Subtitle";
+export { default as Text } from "./Text";
+export { default as Code } from "./Code";
+export { default as Link } from "./Link";
+export { default as SectionTitle } from "./SectionTitle";
