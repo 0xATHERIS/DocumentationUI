385f76ee637ea18a93cdc7630b37e47dbb153b13
0xATHERIS <ath3ris@proton.me>
2023-05-22 00:05:26 +0100

    feat(Subtitle): :sparkles:  - Added className

--- src/components/Subtitle/Subtitle.tsx
+++ src/components/Subtitle/Subtitle.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import cn from '../../utils/cn';
 
 type SubtitleProps = {
     children: React.ReactNode;
+    className?: string;
 };
 
-const Subtitle = React.forwardRef<HTMLHeadingElement, SubtitleProps>(({ children, ...props }, ref) => (
-    <h4 ref={ref} className="text-xl font-medium tracking-tight mb-4 text-neutral-800" {...props}>
+const Subtitle = React.forwardRef<HTMLHeadingElement, SubtitleProps>(({ children, className, ...props }, ref) => (
+    <h2 ref={ref} className={cn("mb-6 text-xl font-medium tracking-tight text-neutral-800 dark:text-neutral-100", className)} {...props}>
         {children}
-    </h4>
+    </h2>
 ));
 
 export default Subtitle;
