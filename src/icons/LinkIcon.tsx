8d0791ffe23a7813cf295ac6552d381d78761f0f
0xATHERIS <ath3ris@proton.me>
2023-05-22 00:07:41 +0100

    feat(icons): :sparkles:  - Link icon

---
+++ src/icons/LinkIcon.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function LinkIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
+            <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="4">
+                <path d="m26.24 16.373l-9.14-9.14c-2.661-2.661-7.035-2.603-9.768.131c-2.734 2.734-2.793 7.107-.131 9.768l7.935 7.936m17.767-2.065l7.935 7.935c2.661 2.662 2.603 7.035-.13 9.769c-2.735 2.734-7.108 2.792-9.77.13l-9.14-9.14"></path>
+                <path d="M26.11 26.142c2.733-2.734 2.792-7.108.13-9.769m-4.441 5.425c-2.734 2.734-2.792 7.108-.131 9.769"></path>
+            </g>
+        </svg>
+    );
+}
