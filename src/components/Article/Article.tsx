c8c020fa1b6b3ebeddac28b678fb13ce6f3a7203
0xATHERIS <ath3ris@proton.me>
2023-05-21 23:47:45 +0100

    refactor: :recycle:  - Added cn util

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import cn from '../../utils/cn';
 
 type ArticleProps = {
     children: React.ReactNode;
+    className?: string;
 };
 
-const Article = React.forwardRef<HTMLElement, ArticleProps>(({ children, ...props }, ref) => (
-    <article ref={ref} className="w-full md:w-3/4 lg:w-1/2 mx-auto px-4" {...props}>
+const Article = React.forwardRef<HTMLElement, ArticleProps>(({ children, className, ...props }, ref) => (
+    <article ref={ref} className={cn("w-full md:w-3/4 lg:w-1/2 mx-auto px-4", className)} {...props}>
         {children}
     </article>
 ));
