dc78e3b9526f900c005dba063836e28a01476175
0xATHERIS <ath3ris@proton.me>
2023-05-22 00:20:44 +0100

    refactor(SectionTitle): :lipstick:  - Added margin top and increase margin bottom by 1

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -8,11 +8,13 @@ type SectionTitleProps = {
 };
 
 const SectionTitle = React.forwardRef<HTMLHeadingElement, SectionTitleProps>(({ children, className, ...props }, ref) => (
-    <a href={`#${children}`} className="flex items-center mb-2 group">
-        <h4 ref={ref} className={cn("mr-1 text-lg font-medium leading-none text-neutral-800 dark:text-neutral-100", className)} {...props}>
+    <a href={`#${children}`} className="flex items-center mb-3 group">
+        <h4 ref={ref} className={cn("mr-1 text-lg font-medium leading-none text-neutral-800 dark:text-neutral-100 mt-4", className)} {...props}>
             {children}
         </h4>
-        <span className="transition-all -translate-x-3 opacity-0 group-hover:opacity-50 group-hover:translate-x-0"><LinkIcon /></span>
+        <span className="mt-4 transition-all -translate-x-3 opacity-0 group-hover:opacity-50 group-hover:translate-x-0">
+            <LinkIcon />
+        </span>
     </a>
 ));
 
