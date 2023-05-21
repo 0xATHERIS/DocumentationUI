1d72f188c0599d314d4e05f77290776bc6ff34e9
0xATHERIS <ath3ris@proton.me>
2023-05-22 00:05:47 +0100

    feat(Title): :sparkles:  - Added className

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from '../../utils/cn';
 
 type TitleProps = {
     children: React.ReactNode;
@@ -6,7 +7,7 @@ type TitleProps = {
 };
 
 const Title = React.forwardRef<HTMLHeadingElement, TitleProps>(({ children, className, ...props }, ref) => (
-    <h1 ref={ref} className={`text-4xl font-bold tracking-tight text-neutral-800 mt-4 mb-2 ${className}`} {...props}>
+    <h1 ref={ref} className={cn('text-4xl font-bold tracking-tight text-neutral-800 mt-4 mb-2 dark:text-neutral-100', className)} {...props}>
         {children}
     </h1>
 ));
